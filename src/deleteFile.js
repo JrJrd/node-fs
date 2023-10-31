eba37ac2b848b3aaa94a4ad7ad71485378ad8c18
seaer <jrd.seaer@gmail.com>
2023-10-31 04:59:46 -0400

    unlink

--- src/deleteFile.js
+++ src/deleteFile.js
@@ -1,1 +1,10 @@
-const fs = require("fs");
+const { unlink } = require("fs");
+
+unlink("./HelloWorld.txt", (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+       
+        console.log("done");
+    }
+});
